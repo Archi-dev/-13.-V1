c7356bf457d26a07d81e6e67406191e9b12a4e07
Archi-dev <gwsm14r@gmail.com>
2021-07-05 12:32:38 +0300

    Update script.js

--- HM13/js/script.js
+++ HM13/js/script.js
@@ -1,18 +1,17 @@
 
-
-function calcNumber(num1, num2 = 1) {
-    console.log( num1);
-    console.log( num2,typeof num2);
-    let finalCalc = Math.pow(num1, num2);
- 
-    return (finalCalc) ? finalCalc : finalCalc = 'some error';
-
+num1 = +prompt('Введите число',"");
+num2 = +prompt('Введите степень',"");
+function calcNumber(num1, num2) {
+    if (isNaN(num1) || isNaN(num2) ||
+        num1 === null || num2 === null||
+        typeof num1 === "string" ||typeof num2 === "string"||
+        num1===""||num2===""){
+      return 'error: Вы ввели неправильные данные';
+    } else {
+       
+        return Math.pow(num1, num2);
     
-};
-
-num1 = prompt('Введите число');
-num2 = +prompt('Введите степень',);
-
-
+}
+}
 let result = calcNumber(num1,num2);
-alert('Результат ' + result);
+alert(result);
